eefd85aa0e1bc3efc33dd20f7dc84900b7af44ab
Studart Erico <erico.studart@bcg.com>
2026-04-19 08:59:24 -0300

    feat: enhance layout

--- src/components/Stream.tsx
+++ src/components/Stream.tsx
@@ -1,7 +1,9 @@
 import useWebSocket from "../hooks/useWebSocket";
 
 export default function Stream() {
-    const { lastMessage } = useWebSocket('ws://192.168.68.100:8000/v1/ws/subscribe?channel=5143470272');
+    const { lastMessage } = useWebSocket(
+        'ws://192.168.68.100:8000/v1/ws/consume?channel=4943361472'
+    );
 
     return (
         <div>
@@ -10,7 +12,7 @@ export default function Stream() {
             <img
             src={`data:image/jpeg;base64,${lastMessage}`}
             alt="stream"
-            style={{ width: "600px" }}
+            style={{ width: "1300px" }}
             />
         ) : (
             <p>Waiting for stream...</p>
